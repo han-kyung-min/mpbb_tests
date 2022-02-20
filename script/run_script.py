cdaed7903fef02b32770038ae9ed48813cb29cde
Kyungmin Han <thezest76@gmail.com>
2022-02-20 11:27:27 +0900

    modified python script (auto arg)

--- script/run_script.py
+++ script/run_script.py
@@ -17,26 +17,33 @@ import shutil
 
 def main(argv):
     
+    if len(sys.argv) != 3:
+        print "usage: {} <num_rounds> <num_threads>".format(argv[0])
+        return -1
+    
     global exploration_status
     num_rounds  = int(argv[1])
+    num_threads = int(argv[2])
 
     #num_thread_dev=[1,2,4,6,8,10,12,14,16]
-    for num_threads in range(1,17):#num_thread_dev:
+    for num_threads in range(1,num_threads+1):#num_thread_dev:
     
         for roundidx in range(1,num_rounds+1):
             
             start_time = time.time()
             elapsed_time = time.time() - start_time
             
+            homedir = os.getenv("HOME")
+            
             global exploration_status
 
-            os.system('/home/hankm/binary_ws/mpbb_tests/bin/mpbb_run {}'.format(num_threads))
+            os.system('{}/binary_ws/mpbb_tests/bin/mpbb_run {}'.format(homedir,num_threads))
 
-            outfiletxt = '/home/hankm/results/autoexploration/mpbb/planning_time_{}_{}.txt'.format(num_threads,roundidx)
-            shutil.copy('/home/hankm/results/autoexploration/planning_time.txt', outfiletxt)
+            outfiletxt = '{}/results/autoexploration/mpbb/planning_time_{}_{}.txt'.format(homedir,num_threads,roundidx)
+            shutil.copy('{}/results/autoexploration/planning_time.txt'.format(homedir), outfiletxt)
 
-            cpuprofiletxt = '/home/hankm/results/autoexploration/cpu_utility/threadutility_{}_{}.txt'.format(num_threads, roundidx)
-            shutil.copy('/home/hankm/results/autoexploration/threadutility.txt',cpuprofiletxt)
+            cpuprofiletxt = '{}/results/autoexploration/cpu_utility/threadutility_{}_{}.txt'.format(homedir,num_threads, roundidx)
+            shutil.copy('{}/results/autoexploration/threadutility.txt'.format(homedir),cpuprofiletxt)
             time.sleep(1)
     
 if __name__ == '__main__':
